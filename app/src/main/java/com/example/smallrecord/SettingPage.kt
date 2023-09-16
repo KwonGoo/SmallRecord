c0dfd4e08e7d5a0106cd50836e7fa061619cee0b
kimye <rladpdms74@gmail.com>
2023-09-17 00:42:25 +0900

    설정 페이지1.2

--- app/src/main/java/com/example/smallrecord/SettingPage.kt
+++ app/src/main/java/com/example/smallrecord/SettingPage.kt
@@ -1,14 +1,19 @@
 package com.example.smallrecord
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.ActionBar
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Switch
 
 class SettingPage : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +54,22 @@ class SettingPage : AppCompatActivity() {
             val intent = Intent(this, CommunityPage::class.java)
             startActivity(intent)
             overridePendingTransition(0, 0)
+        }
+
+        val ASwitch = findViewById<Switch>(R.id.ASwitch)
+
+        // SharedPreferences를 초기화
+        sharedPreferences = getSharedPreferences("SwitchSettings", Context.MODE_PRIVATE)
+
+        // 스위치의 초기 상태를 설정
+        val switchState = sharedPreferences.getBoolean("switch_state", false)
+        ASwitch.isChecked = switchState
+
+        // 스위치의 상태가 변경될 때 SharedPreferences에 저장
+        ASwitch.setOnCheckedChangeListener { _, isChecked ->
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("switch_state", isChecked)
+            editor.apply()
         }
 
 
