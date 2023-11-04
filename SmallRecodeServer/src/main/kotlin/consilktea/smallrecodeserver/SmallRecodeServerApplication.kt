cc950ffb0b3625e865ff1cb412d26e96c9e02075
kimjaesung <barbo9510@icloud.com>
2023-11-04 15:12:23 +0900

    SmallRecodeServer 구축

---
+++ SmallRecodeServer/src/main/kotlin/consilktea/smallrecodeserver/SmallRecodeServerApplication.kt
@@ -0,0 +1,11 @@
+package consilktea.smallrecodeserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmallRecodeServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmallRecodeServerApplication>(*args)
+}
---
+++ SmallRecodeServer/src/test/kotlin/consilktea/smallrecodeserver/SmallRecodeServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package consilktea.smallrecodeserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmallRecodeServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
